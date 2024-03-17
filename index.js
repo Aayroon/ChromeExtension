fa4d9dd995ea1e5fbe1a0fcbc6d2a4e212ff5e6b
Aayroon <aaronitez4@gmail.com>
2024-03-17 11:22:21 +0300

    Render the leads in an unordered list

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
+let myLeads = ["www.myleads.com", "www.example.com", "www.euro.com"]
 
+const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+for (i=0; i<myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
