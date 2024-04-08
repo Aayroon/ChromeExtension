a636ccce6981b8d4726230d6907521c59dea7c92
Aayroon <aaronitez4@gmail.com>
2024-04-08 09:40:58 +0300

    Deployment

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (i=0; i<myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        //listItems += "<li><a href='"+ myLeads[i] +"' target='_blank'>" + myLeads[i] + "</a></li>"
+        listItems += `<li>
+                        <a href='${myLeads[i]}' target='_blank'>
+                        ${myLeads[i]}
+                        </a>
+                      </li>`
     }
     ulEl.innerHTML = listItems
 }
